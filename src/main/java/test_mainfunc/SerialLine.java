fbe15530046d5f0ad86bf86cf5d39e33820efaa4
ericapastore <89erica89>
2019-02-20 10:17:27 +0100

    All Benders function implemented

--- src/main/java/test_mainfunc/SerialLine.java
+++ src/main/java/test_mainfunc/SerialLine.java
@@ -1,9 +1,17 @@
 package test_mainfunc;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.io.InputStream;
 import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
+
+import ilog.concert.*;
+import ilog.cplex.*;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
 // import org.apache.commons.math3.distribution.UniformRealDistribution;
@@ -15,12 +23,28 @@ public class SerialLine {
     public int NbStage;
     public int[] Buffer;
     public StochNum[] CT;
+    public int Uj;
+    public double THstar;
 
     //Sim_Output
     public double OverallCT;
+    public double TH;
     public double[] Pblock;
     public double[] Pstarve;
+    public int Maxit = 100;
 
+    //cplex public variables
+    public IloCplex cplex;
+    //ERICA: CHECK IF NEEDED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    public IloNumVar[] BJ;
+    public IloNumVar[][] DeltaBJrp;
+    public IloNumVar[][] DeltaBJrm;
+    public IloLinearNumExpr objective;
+    public int BJsol[][];
+    public double[][][] ubarij;
+    public double[][][] wbarij;
+    public double thetabar;
+    public int totit;
 
 
     // Create a new test_mainfunc.SerialLine from txt file
@@ -33,6 +57,9 @@ public class SerialLine {
 
         scanner.next();
         this.NbStage=scanner.nextInt();
+
+        scanner.next();
+        this.Uj=scanner.nextInt();
 
         this.Buffer = new int[this.NbStage - 1];
         scanner.next();
@@ -123,9 +150,12 @@ public class SerialLine {
             int[][] vij,
             int[][] wij,
             int[][] sij,
+            double thetabar,
             double[][] bar_Sij,
             double[] bar_Dij,
-            boolean SteadyState) {
+            int iter,
+            boolean SteadyState,
+            boolean bendersuse) {
         //*****************************************************************************
         //*****************************************************************************
         //** The notations of u,v,w,s is consistent with the journal paper ************
@@ -176,7 +206,12 @@ public class SerialLine {
             }
         }
 
-
+        if(bendersuse)
+        {
+            for(int j=0;j< NbStage -1;j++){
+                Buffer[j]=this.BJsol[j][iter];
+            }
+        }
 
         if (!SteadyState) {
 
@@ -300,6 +335,12 @@ public class SerialLine {
 
                 }
             }
+
+            for(int j=0;j<NbStage-1;j++)
+            {
+                System.out.println("buffer at " + j + " is: " + Buffer[j]);
+            }
+
             this.OverallCT = (Dij[N - 1][NbStage - 1] - Dij[W - 1][NbStage - 1]) / (double)(N - W);
             //***************   SAVE bar_Sij, bar_Dij    ***************
             for (int j = 0; j < NbStage; j++) {
@@ -421,9 +462,281 @@ public class SerialLine {
                 }
             }
             this.OverallCT = (Dij[N-1][NbStage-1]- Dij[W-1][NbStage-1])/ (double)(N-W);
+            this.ubarij = new double [N][NbStage][this.Maxit];
+            this.wbarij = new double [N][NbStage][this.Maxit];
+            for (int j=0; j<this.NbStage;j++)
+            {
+                for(int i=0; i<N;i++)
+                {
+                    this.ubarij[i][j][iter]=uij[i][j];
+                    this.wbarij[i][j][iter]=wij[i][j];
+                }
+            }
+            /////////////////DEFINE HOW MUCH IS THETA FROM THE SIMULATION
+            thetabar=1;
         }
 
 
     }
+
+    public void solveBender(
+            int jobs,
+            int W,
+            double[][] tij,
+            int[][] uij,
+            int[][] vij,
+            int[][] wij,
+            int[][] sij,
+            double[][] bar_Sij,
+            double[] bar_Dij,
+            boolean Stolletz) {
+
+        double theta = 0;
+
+        try {
+
+            //run master problem
+            this.MasterBenders();
+            //run simulation (as subproblem)
+            int numit = 0;
+            this.SIM_Serial_BAS(jobs,W,tij,uij,vij,wij,sij,theta,bar_Sij,bar_Dij, numit,false,true);
+            System.out.println("CT is:" + this.OverallCT );
+            this.TH = 1/this.OverallCT;
+            while((this.TH < this.THstar) && (numit < this.Maxit-1))
+            {
+                numit++;
+                this.AddFeasibilityCut(jobs,numit, tij, theta, Stolletz);
+                this.SIM_Serial_BAS(jobs,W,tij,uij,vij,wij,sij,theta,bar_Sij,bar_Dij,numit,false,true);
+                this.TH = 1/this.OverallCT;
+                System.out.println("simulated TH" + this.TH );
+            }
+            this.totit = numit;
+
+            cplex.end();
+
+            System.out.println("num. iteraz: " + numit);
+        }
+
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+    }
+
+    public void MasterBenders() {
+
+        int J = this.NbStage;
+        int Uj = this.Uj;
+
+        try {
+            //E: Model definition
+            cplex = new IloCplex();
+
+            //E: Variable definition
+
+            BJ = cplex.intVarArray(J-1,0, Uj);
+            DeltaBJrp = new IloNumVar[J-1][];
+
+            for (int j=0; j<J-1; j++)
+            {
+                DeltaBJrp[j] = cplex.intVarArray(this.Maxit,0,Uj);
+            }
+            DeltaBJrm = new IloNumVar[J-1][];
+            for (int j=0; j<J-1; j++)
+            {
+                DeltaBJrm[j] = cplex.intVarArray(this.Maxit,0,Uj);
+            }
+
+            //E: objective function
+            this.objective = cplex.linearNumExpr();
+            for (int j=0; j<J-1; j++)
+            {
+                this.objective.addTerm(1,BJ[j]);
+                for(int r = 0;r<this.Maxit;r++){
+                    this.objective.addTerm(1, DeltaBJrp[j][r]);
+                    this.objective.addTerm(1, DeltaBJrm[j][r]);
+                }
+            }
+            cplex.addMinimize(this.objective);
+
+            //E: Delta definition constraints (22)
+//            for (int j=0; j<J-1; j++)
+////            {
+////                    IloLinearNumExpr sumBJ_expr = cplex.linearNumExpr();
+////                    IloRange rng;
+////                    sumBJ_expr.addTerm(1,DeltaBJrp[j][0]);
+////                    sumBJ_expr.addTerm(-1,BJ[j]);
+////                    rng = cplex.addGe(sumBJ_expr, -1*BJsol[j][0]);
+////                    rng.setName("deltaBJp" + j + "0");
+////
+////            }
+////
+////            //E: Delta definition constraints (23)
+////            for (int j=0; j<J-1; j++)
+////            {
+////                    IloLinearNumExpr sumDeltaBJ_expr = cplex.linearNumExpr();
+////                    IloRange rng;
+////                    sumDeltaBJ_expr.addTerm(1,DeltaBJrm[j][0]);
+////                    sumDeltaBJ_expr.addTerm(1,BJ[j]);
+////                    rng = cplex.addGe(sumDeltaBJ_expr, BJsol[j][0]);
+////                    rng.setName("deltaBJm" + j + "0");
+////            }
+
+            //E: RISOLUZIONE DEL MODELLO
+            if (cplex.solve())
+            {
+                System.out.println("obj = "+cplex.getObjValue());
+                //finished=true;
+
+                //save current solution
+                this.BJsol = new int[J-1][this.Maxit];
+                for(int j=0;j<J-1;j++)
+                {
+                    //System.out.println(cplex.getValue(this.BJ[j]));
+                    this.BJsol[j][0] = (int) cplex.getValue(this.BJ[j]);
+                }
+            }
+            else
+            {
+                System.out.println("problem not solved");
+            }
+
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+    }
+
+    public void AddFeasibilityCut(
+            int jobs,
+            int numint,
+            double[][] tij,
+            double thetabar,
+            boolean Stolletz) {
+
+        int J = this.NbStage;
+        int n = jobs;
+        //i define the last already explored iteration as nnint
+        int nnint = numint -1;
+
+        try {
+            ////E: insert new nnint-delta in objective function
+            //this.objective = cplex.linearNumExpr();
+//            for (int j=0; j<J-1; j++)
+//            {
+//                this.objective.addTerm(1, DeltaBJrp[j][nnint]);
+//                this.objective.addTerm(1, DeltaBJrm[j][nnint]);
+//            }
+//            cplex.addMinimize(this.objective);
+
+
+            //E: Delta definition constraints (22)
+            for (int j=0; j<J-1; j++)
+            {
+                IloLinearNumExpr sumBJ_expr = cplex.linearNumExpr();
+                IloRange rng;
+                sumBJ_expr.addTerm(1,DeltaBJrp[j][nnint]);
+                sumBJ_expr.addTerm(-1,BJ[j]);
+                /////////////////CHECK THE CORRECT ITERATION OF BJSOL
+                rng = cplex.addGe(sumBJ_expr, -1*BJsol[j][nnint]);
+                rng.setName("deltaBJp" + j + nnint);
+
+            }
+
+            //E: Delta definition constraints (23)
+            for (int j=0; j<J-1; j++)
+            {
+                IloLinearNumExpr sumDeltaBJ_expr = cplex.linearNumExpr();
+                IloRange rng;
+                sumDeltaBJ_expr.addTerm(1,DeltaBJrm[j][nnint]);
+                sumDeltaBJ_expr.addTerm(1,BJ[j]);
+                /////////////////CHECK THE CORRECT ITERATION OF BJSOL
+                rng = cplex.addGe(sumDeltaBJ_expr, BJsol[j][nnint]);
+                rng.setName("deltaBJm" + j + nnint);
+            }
+
+            if(!Stolletz)
+            {
+                double[] DeltapPar;
+                double[] DeltamPar;
+                DeltapPar = new double[J-1];
+                DeltamPar = new double[J-1];
+
+                mM_value mm = new mM_value(J, n, this.Uj, 0);
+
+                for(int j=0;j<J -1;j++)
+                {
+                    for (int i=0;i<n;i++)
+                    {
+                        DeltapPar[j]= DeltapPar[j] + this.wbarij[i][j][nnint]*mm.Mijk[i][j][BJsol[j][nnint]];
+                        DeltamPar[j]= DeltamPar[j] + this.wbarij[i][j][nnint]*mm.mijk[i][j][BJsol[j][nnint]];
+                    }
+                }
+
+                double tijpar=0.0;
+                for(int j=0;j<J -1;j++)
+                {
+                    for (int i=0;i<n;i++)
+                    {
+                        tijpar = tijpar + tij[i][j]*this.ubarij[i][j][nnint];
+                    }
+                }
+
+                double thetapar = thetabar/this.THstar;
+
+
+                IloLinearNumExpr sumBJcut_expr = cplex.linearNumExpr();
+                IloRange rng;
+                for (int j=0; j<J-1; j++)
+                {
+                    sumBJcut_expr.addTerm(-DeltapPar[j],DeltaBJrp[j][nnint]);
+                    sumBJcut_expr.addTerm(-DeltamPar[j],DeltaBJrm[j][nnint]);
+                }
+                double resc = thetapar - tijpar;
+                rng = cplex.addLe(sumBJcut_expr,resc);
+                rng.setName("feascut of iter"+nnint);
+
+            }
+            if(Stolletz)
+            {
+                IloLinearNumExpr sumBJcut_expr = cplex.linearNumExpr();
+                IloRange rng;
+                int lastcapsum = 0;
+                for(int j=0;j< J-1;j++)
+                {
+                    sumBJcut_expr.addTerm(1,BJ[j]);
+                    lastcapsum = lastcapsum + this.BJsol[j][nnint];
+                }
+                lastcapsum=lastcapsum+1;
+                rng = cplex.addGe(sumBJcut_expr,lastcapsum);
+                rng.setName("combcut of iter"+nnint);
+            }
+
+            //E: Model solution
+            if (cplex.solve())
+            {
+                System.out.println("obj = "+cplex.getObjValue());
+                //finished=true;
+                System.out.println("obj = "+cplex.getObjValue());
+
+                //save current solution
+                for(int j=0;j<this.NbStage-1;j++)
+                {
+                    this.BJsol[j][nnint+1] = (int) cplex.getValue(this.BJ[j]);
+                }
+
+            }
+            else
+            {
+                System.out.println("problem not solved");
+            }
+
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+
 
 }
--- src/main/java/test_mainfunc/Test_SerialLineSimulation.java
+++ src/main/java/test_mainfunc/Test_SerialLineSimulation.java
@@ -1,6 +1,9 @@
 package test_mainfunc;
 
 import java.io.*;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 
 import static java.lang.Math.max;
 
@@ -43,6 +46,7 @@ public class Test_SerialLineSimulation {
         int[][] wij=new int[N][mySystem.NbStage];
         int[][] sij=new int[N][mySystem.NbStage];
         double[] bar_Dij=new double[mySystem.NbStage];
+        double thetapar=0.0;
         int B = 0;
         for (int j = 0; j < mySystem.NbStage - 1; j++) {
             B = max(B, mySystem.Buffer[j]);
@@ -50,17 +54,42 @@ public class Test_SerialLineSimulation {
         double[][] bar_Sij = new double [B][mySystem.NbStage];
 
         mySystem.ProcTimeGeneration(N,tij);
-        mySystem.SIM_Serial_BAS(N,W,tij,uij,vij,wij,sij,bar_Sij,bar_Dij,false);
+        mySystem.SIM_Serial_BAS(N,W,tij,uij,vij,wij,sij,thetapar,bar_Sij,bar_Dij,0,false, false);
 
         PrintWriter writer = new PrintWriter(outRes, true);
         // cycle time is calculated between W and N, whatever value SteadyState will take.
         writer.print("Cycle time with simulation from 0: ");
         writer.println(mySystem.OverallCT);
 
-        mySystem.SIM_Serial_BAS(N,W,tij,uij,vij,wij,sij,bar_Sij,bar_Dij,true);
+        mySystem.SIM_Serial_BAS(N,W,tij,uij,vij,wij,sij,thetapar,bar_Sij,bar_Dij,0,false,false );
         writer.print("Cycle time with simulation from steady state: ");
         writer.println(mySystem.OverallCT);
 
+
+        //optimization part
+        long StartOpt = System.currentTimeMillis();
+        mySystem.solveBender(N,W,tij,uij,vij,wij,sij,bar_Sij,bar_Dij,true);
+        long elapsedTimeMillisC = System.currentTimeMillis()- StartOpt;
+        float elapsedTimeSecC = elapsedTimeMillisC/1000F;
+        double OptTime = elapsedTimeSecC;
+
+        writer.print("Cycle time with Benders Decomposition: ");
+        double tempCT=1/mySystem.TH;
+        writer.println(tempCT);
+
+        DecimalFormat df = new DecimalFormat("#.#####");
+        df.setRoundingMode(RoundingMode.CEILING);
+        writer.write("total time CG: ");
+        writer.write(df.format(OptTime));
+
+        writer.print("Optimal Buffer: ");
+        for(int j=0;j<mySystem.NbStage-1;j++){
+            writer.println(mySystem.BJsol[j][mySystem.totit]);
+            writer.println(" ");
+        }
+
+        writer.print("Total number of iterations: ");
+        writer.println(mySystem.totit);
 
         try {
             outRes.close();
